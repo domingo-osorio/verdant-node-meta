b5f9c8d66e1482ee6fb90dd164a809a8f31ebd98
domingo-osorio <vindaloo.gamedev@gmail.com>
2025-09-18 20:34:41 +0200

    remove not needed parameter at request

--- import.js
+++ import.js
@@ -7,12 +7,12 @@ const { SPREADSHEET_ID: spreadsheetId, OUTPUT_FILE, JWT } = process.env;
 const auth = new google.auth.GoogleAuth({ keyFile: JWT, scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"] });
 const client = await auth.getClient();
 const google_sheets = google.sheets({ version: "v4", auth: client });
-const meta_data = await google_sheets.spreadsheets.get({ auth, spreadsheetId });
+const meta_data = await google_sheets.spreadsheets.get({ spreadsheetId });
 const sheet_names = meta_data.data.sheets.map(x => x.properties.title);
 
 let entries = await Promise.all(
     sheet_names.map(async x => {
-        const sheet_data = await google_sheets.spreadsheets.values.get({ auth, spreadsheetId, range: x + "!A:ZZZ" });
+        const sheet_data = await google_sheets.spreadsheets.values.get({ spreadsheetId, range: x + "!A:ZZZ" });
         return [x, sheet_data.data.values];
     })
 );
